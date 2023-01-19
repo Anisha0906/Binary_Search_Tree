68e2c25cd7e36d8e7412c44ab32ad398316ab552
Anisha0906 <anishadas880@gmail.com>
2023-01-19 22:54:48 +0530

    [Anisha]Added:Ability to create a BST by adding 56 and then adding 30 & 70

---
+++ BinarySearchTree/Binarysearchtree.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    internal class Binarysearchtree<T> where T : IComparable
+    {
+        T NodeData;
+        public Binarysearchtree<T> leftTree { get; set; }
+        public Binarysearchtree<T> rightTree { get; set; }
+        bool result = false;
+
+        public Binarysearchtree(T NodeData)
+        {
+            this.NodeData = NodeData;
+            this.leftTree = null;
+            this.rightTree = null;
+        }
+        public void Insert(T item)
+        {
+            T currentNodeValue = this.NodeData;
+            if ((currentNodeValue.CompareTo(item)) > 0)
+            {
+                if (this.leftTree == null)
+                    this.leftTree = new Binarysearchtree<T>(item);
+                else
+                    this.leftTree.Insert(item);
+            }
+            else
+            {
+                if (this.rightTree == null)
+                    this.rightTree = new Binarysearchtree<T>(item);
+                else
+                    this.rightTree.Insert(item);
+            }
+        }
+        public void Display()
+        {
+            if (this.leftTree != null)
+            {
+                this.leftTree.Display();
+            }
+            Console.WriteLine(this.NodeData.ToString());
+            if (this.rightTree != null)
+            {
+                this.rightTree.Display();
+            }
+        }
+    }
+}
---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Binarysearchtree<int> binarySearchTree = new Binarysearchtree<int>(56);
+            binarySearchTree.Insert(30);
+            binarySearchTree.Insert(70);
+            Console.WriteLine("Binary Search Tree -");
+            binarySearchTree.Display();
+            Console.ReadLine();
+        }
+    }
+}
